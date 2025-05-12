d7d76aa1dec9d3f5d65109754156c4ca2d9bd5b1
shuiruge <shuiruge@whu.edu.cn>
2025-05-12 11:03:56 +0800

    Examine the Newton's direction versus the direction of gradient.

---
+++ experiments/mlp.py
@@ -0,0 +1,208 @@
+# Following: https://docs.jax.dev/en/latest/notebooks/Neural_Network_and_Data_Loading.html
+
+
+# -------- Dataset --------- #
+
+import numpy as np
+import jax.numpy as jnp
+from jax.tree_util import tree_map
+from torch.utils.data import DataLoader, default_collate
+from torchvision.datasets import MNIST
+from torchvision.transforms import Resize
+
+# We decrease the image size for reducing dimension.
+image_size = 8
+batch_size = 128
+n_targets = 10
+
+def numpy_collate(batch):
+  """
+  Collate function specifies how to combine a list of data samples into a batch.
+  default_collate creates pytorch tensors, then tree_map converts them into numpy arrays.
+  """
+  return tree_map(np.asarray, default_collate(batch))
+
+def resize_flatten_cast(pic):
+  """Convert PIL image to flat (1-dimensional) numpy array."""
+  pic = Resize(size=image_size)(pic)
+  return np.ravel(np.array(pic, dtype=jnp.float32))
+
+def one_hot(x, k, dtype=jnp.float32):
+  """Create a one-hot encoding of x of size k."""
+  return jnp.array(x[:, None] == jnp.arange(k), dtype)
+
+# Define our dataset, using torch datasets
+mnist_dataset = MNIST('/tmp/mnist/', download=True, transform=resize_flatten_cast)
+# Create pytorch data loader with custom collate function
+training_generator = DataLoader(mnist_dataset, batch_size=batch_size, collate_fn=numpy_collate)
+
+# Get the full train dataset (for checking accuracy while training)
+train_images = np.array(Resize(size=image_size)(mnist_dataset.train_data)).reshape(len(mnist_dataset.train_data), -1)
+train_labels = one_hot(np.array(mnist_dataset.train_labels), n_targets)
+
+# Get full test dataset
+mnist_dataset_test = MNIST('/tmp/mnist/', download=True, train=False)
+test_images = jnp.array(Resize(size=image_size)(mnist_dataset_test.test_data).numpy().reshape(len(mnist_dataset_test.test_data), -1), dtype=jnp.float32)
+test_labels = one_hot(np.array(mnist_dataset_test.test_labels), n_targets)
+
+
+# -------- Multi-Layer Perceptron --------- #
+
+from jax import grad, jit, vmap
+from jax import random
+
+# A helper function to randomly initialize weights and biases
+# for a dense neural network layer
+def random_layer_params(m, n, key, scale=1e-2):
+  w_key, b_key = random.split(key)
+  return scale * random.normal(w_key, (n, m)), scale * random.normal(b_key, (n,))
+
+# Initialize all layers for a fully-connected neural network with sizes "sizes"
+def init_network_params(sizes, key):
+  keys = random.split(key, len(sizes))
+  params = []
+  for m, n, k in zip(sizes[:-1], sizes[1:], keys):
+    params += random_layer_params(m, n, k)
+  return params
+
+layer_sizes = [image_size*image_size, 128, 10]
+step_size = 0.01
+num_epochs = 2
+params = init_network_params(layer_sizes, random.key(0))
+
+from jax.scipy.special import logsumexp
+
+def relu(x):
+  return jnp.maximum(0, x)
+
+def predict(params, image):
+  grouped_params = [(params[2*i], params[2*i+1]) for i in range(len(params)//2)]
+
+  # per-example predictions
+  activations = image
+  for w, b in grouped_params[:-1]:
+    outputs = jnp.dot(w, activations) + b
+    activations = relu(outputs)
+
+  final_w, final_b = grouped_params[-1]
+  logits = jnp.dot(final_w, activations) + final_b
+  return logits - logsumexp(logits)
+
+# Make a batched version of the `predict` function
+batched_predict = vmap(predict, in_axes=(None, 0))
+
+def accuracy(params, images, targets):
+  target_class = jnp.argmax(targets, axis=1)
+  predicted_class = jnp.argmax(batched_predict(params, images), axis=1)
+  return jnp.mean(predicted_class == target_class)
+
+def loss(params, images, targets):
+  preds = batched_predict(params, images)
+  return -jnp.mean(preds * targets)
+
+@jit
+def update(params, x, y):
+  grads = grad(loss)(params, x, y)
+  return [p - step_size * g for (p, g) in zip(params, grads)]
+
+
+# -------- Optimization --------- #
+
+import time
+
+for epoch in range(num_epochs):
+  start_time = time.time()
+  for x, y in training_generator:
+    y = one_hot(y, n_targets)
+    params = update(params, x, y)
+  epoch_time = time.time() - start_time
+
+  train_acc = accuracy(params, train_images, train_labels)
+  test_acc = accuracy(params, test_images, test_labels)
+  print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
+  print("Training set accuracy {}".format(train_acc))
+  print("Test set accuracy {}".format(test_acc))
+
+
+# -------- Visualization --------- #
+
+import jax
+import matplotlib.pyplot as plt
+
+def normalize(vector, eps=0.):
+    return vector / (np.sqrt(np.sum(np.square(vector))) + eps)
+
+inputs, targets = next(iter(training_generator))
+_, grads = jax.value_and_grad(loss)(params, inputs, one_hot(targets, n_targets))
+for grad in grads:
+    grad = np.reshape(grad, [-1])
+    normalized_grad = normalize(grad)
+    print(f'#elements = {normalized_grad.shape[0]}, ',
+          f'max = {np.max(normalized_grad)}, ',
+          f'min = {np.min(normalized_grad)}')
+    plt.hist(normalized_grad, bins=50, density=True)
+    plt.show()
+
+
+def get_param_shapes_and_boundaries(params):
+  shapes = []
+  boundaries = []
+  offset = 0
+  for p in params:
+    offset += jnp.reshape(p, [-1]).shape[0]
+    boundaries.append(offset)
+    shapes.append(p.shape)
+  return shapes, boundaries
+
+def concat_params(params):
+  return jnp.concat([jnp.reshape(p, [-1]) for p in params])
+
+def split_params(concated_params, shapes, boundaries):
+  params = []
+  i = 0
+  for shape, offset in zip(shapes, boundaries):
+    #print(i, offset, shape)
+    #print(concated_params[i:offset].shape)
+    p = jnp.reshape(concated_params[i:offset], shape)
+    i = offset
+    params.append(p)
+  return params
+
+def get_mlp_grad(params, inputs, targets):
+  shapes, boundaries = get_param_shapes_and_boundaries(params)
+
+  def f(concated_params):
+    params = split_params(concated_params, shapes, boundaries)
+    return loss(params, inputs, targets)
+
+  g = jax.grad(f)
+  return g(concat_params(params))
+
+def hessian(f):
+  return jax.jacrev(jax.grad(f))
+
+def get_mlp_hessian(params, inputs, targets):
+  shapes, boundaries = get_param_shapes_and_boundaries(params)
+
+  def f(concated_params):
+    params = split_params(concated_params, shapes, boundaries)
+    return loss(params, inputs, targets)
+
+  h = hessian(f)
+  return h(concat_params(params))
+
+g = get_mlp_grad(params, inputs, one_hot(targets, n_targets))
+normalized_g = normalize(g)
+
+h = get_mlp_hessian(params, inputs, one_hot(targets, n_targets))
+h.shape
+
+h_inv = jnp.linalg.inv(h + 1e-8 * jnp.eye(h.shape[0]))
+g2 = jnp.dot(h_inv, g)
+normalized_g2 = normalize(g2)
+normalized_g2
+jnp.dot(normalized_g2, normalized_g)
+
+gsign = jnp.sign(g)
+normalized_gsign = normalize(gsign)
+jnp.dot(normalized_g2, normalized_gsign)
--- shell.nix
+++ shell.nix
@@ -1,12 +1,21 @@
 # shell.nix
 with import <nixpkgs> { };
 let
-  pythonPackages = python311Packages;
+  pythonPackages = python312Packages;
 in pkgs.mkShell rec {
   name = "soma-optimizer";
   buildInputs = [
     pythonPackages.python
-    pythonPackages.ipykernel
+    #pythonPackages.ipykernel
     pythonPackages.optax
+
+    # for testing
+    pythonPackages.numpy
+    pythonPackages.jax
+    pythonPackages.flax
+    pythonPackages.matplotlib
+    # torch and torchvision for loading datasets.
+    pythonPackages.torch
+    pythonPackages.torchvision
   ];
 }
