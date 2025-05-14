063db544e69c9f92991410b5eda27499c8fca48f
shuiruge <shuiruge@whu.edu.cn>
2025-05-14 19:58:42 +0800

    Compute total loss difference instead of its second order expansion.

--- experiments/mlp-mnist.py
+++ experiments/mlp-mnist.py
@@ -8,10 +8,8 @@ import jax.numpy as jnp
 from jax.tree_util import tree_map
 from torch.utils.data import DataLoader, default_collate
 from torchvision.datasets import MNIST
-from torchvision.transforms import Resize
 
 # We decrease the image size for reducing dimension.
-image_size = 8
 batch_size = 128
 n_targets = 10
 
@@ -24,7 +22,6 @@ def numpy_collate(batch):
 
 def resize_flatten_cast(pic):
   """Convert PIL image to flat (1-dimensional) numpy array."""
-  pic = Resize(size=image_size)(pic)
   return np.ravel(np.array(pic, dtype=jnp.float32))
 
 def one_hot(x, k, dtype=jnp.float32):
@@ -37,12 +34,12 @@ mnist_dataset = MNIST('/tmp/mnist/', download=True, transform=resize_flatten_cas
 training_generator = DataLoader(mnist_dataset, batch_size=batch_size, collate_fn=numpy_collate)
 
 # Get the full train dataset (for checking accuracy while training)
-train_images = np.array(Resize(size=image_size)(mnist_dataset.train_data)).reshape(len(mnist_dataset.train_data), -1)
+train_images = np.array(mnist_dataset.train_data).reshape(len(mnist_dataset.train_data), -1)
 train_labels = one_hot(np.array(mnist_dataset.train_labels), n_targets)
 
 # Get full test dataset
 mnist_dataset_test = MNIST('/tmp/mnist/', download=True, train=False)
-test_images = jnp.array(Resize(size=image_size)(mnist_dataset_test.test_data).numpy().reshape(len(mnist_dataset_test.test_data), -1), dtype=jnp.float32)
+test_images = jnp.array(mnist_dataset_test.test_data.numpy().reshape(len(mnist_dataset_test.test_data), -1), dtype=jnp.float32)
 test_labels = one_hot(np.array(mnist_dataset_test.test_labels), n_targets)
 
 
@@ -65,9 +62,9 @@ def init_network_params(sizes, key):
     params += random_layer_params(m, n, k)
   return params
 
-layer_sizes = [image_size*image_size, 20, 10]
+layer_sizes = [28*28, 512, 10]
 step_size = 0.01
-num_epochs = 5
+num_epochs = 8
 params = init_network_params(layer_sizes, random.key(0))
 
 from jax.scipy.special import logsumexp
@@ -106,94 +103,35 @@ def update(params, x, y):
   return [p - step_size * g for (p, g) in zip(params, grads)]
 
 
-# -------- Second-Order Correction --------- #
+# -------- Higher-Order Correction --------- #
 
 import jax
 
-def get_param_shapes_and_boundaries(params):
-  shapes = []
-  boundaries = []
-  offset = 0
-  for p in params:
-    offset += jnp.reshape(p, [-1]).shape[0]
-    boundaries.append(offset)
-    shapes.append(p.shape)
-  return shapes, boundaries
-
-def concat_params(params):
-  return jnp.concat([jnp.reshape(p, [-1]) for p in params])
-
-def split_params(concated_params, shapes, boundaries):
-  params = []
-  i = 0
-  for shape, offset in zip(shapes, boundaries):
-    #print(i, offset, shape)
-    #print(concated_params[i:offset].shape)
-    p = jnp.reshape(concated_params[i:offset], shape)
-    i = offset
-    params.append(p)
-  return params
-
-def get_mlp_grad(params, inputs, targets):
-  shapes, boundaries = get_param_shapes_and_boundaries(params)
-
-  def f(concated_params):
-    params = split_params(concated_params, shapes, boundaries)
-    return loss(params, inputs, targets)
-
-  g = jax.grad(f)
-  return g(concat_params(params))
-
-def hessian(f):
-  return jax.jacrev(jax.grad(f))
-
-def get_mlp_hessian(params, inputs, targets):
-  shapes, boundaries = get_param_shapes_and_boundaries(params)
-
-  def f(concated_params):
-    params = split_params(concated_params, shapes, boundaries)
-    return loss(params, inputs, targets)
-
-  h = hessian(f)
-  return h(concat_params(params))
+def loss_diffs(params, inputs, targets, step_size):
+  grads = jax.grad(loss)(params, inputs, targets)
+  new_params = [p - step_size * g for p, g in zip(params, grads)]
+  first_order = -sum([step_size * jnp.sum(jnp.square(g)) for g in grads])
+  total = loss(new_params, inputs, targets) - loss(params, inputs, targets)
+  return first_order, total
 
 
 # -------- Visualization --------- #
 
 import matplotlib.pyplot as plt
 
-def normalize(vector, eps=0.):
-    return vector / (np.sqrt(np.sum(np.square(vector))) + eps)
-
 step = 0
-L1_lst, L2_lst = [], []
+steps, dL1_lst, dL_lst = [], [], []
 for epoch in range(num_epochs):
   for x, y in training_generator:
-    y = one_hot(y, n_targets)
-    params = update(params, x, y)
-
+    params = update(params, x, one_hot(y, n_targets))
     step += 1
     if step % 100 == 0:
-      print(f'step = {step}')
-      #eval_inputs = x
-      #eval_targets = one_hot(y, n_targets)
       eval_inputs = train_images[:5000, :]
       eval_targets = one_hot(train_labels[:5000], n_targets)
-      g = get_mlp_grad(params, eval_inputs, eval_targets)
-      normalized_g = normalize(g)
-      gsign = jnp.sign(g)
-      normalized_gsign = normalize(gsign)
-      # print(f'(H_inv @ g) @ sign(g) = {jnp.dot(normalized_g2, normalized_gsign)}')
-      print(f'g @ sign(g) = {jnp.dot(normalized_g, normalized_gsign)}')
-
-      H = get_mlp_hessian(params, eval_inputs, eval_targets)
-
-      dx = step_size * g
-      L1 = jnp.dot(g, dx)
-      L2 = 0.5 * jnp.dot(dx, H @ dx)
-      print(f'L1 = {L1}, L2 = {L2}')
-      L1_lst.append(L1)
-      L2_lst.append(L2)
+      dL1, dL = loss_diffs(params, eval_inputs, eval_targets, step_size)
+      steps.append(step)
+      dL1_lst.append(dL1)
+      dL_lst.append(dL)
 
   train_acc = accuracy(params, train_images, train_labels)
   test_acc = accuracy(params, test_images, test_labels)
@@ -201,13 +139,13 @@ for epoch in range(num_epochs):
   print("Test set accuracy {}".format(test_acc))
 
 plt.clf()
-plt.plot(L1_lst, label='L1')
-plt.plot(L2_lst, label='L2')
+plt.plot(steps, dL1_lst, label='dL1')
+plt.plot(steps, dL_lst, label='dL')
 plt.legend()
 plt.show()
 
 plt.clf()
-plt.plot([a/b for a, b in zip(L1_lst, L2_lst)], label='L1/L2')
+plt.plot(steps, [a/b for a, b in zip(dL1_lst, dL_lst)], label='dL1/dL')
 plt.legend()
 plt.show()
 
